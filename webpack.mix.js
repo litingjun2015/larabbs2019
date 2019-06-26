64a28f5f39eb0c73b710644f9e98092e1892737d
John Li <tingjun.li@gmail.com>
2019-06-26 15:41:22 +0800

    静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,3 @@
-const mix = require('laravel-mix');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,5 +9,8 @@ const mix = require('laravel-mix');
  |
  */
 
+const mix = require('laravel-mix');
+
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css').version();
+
